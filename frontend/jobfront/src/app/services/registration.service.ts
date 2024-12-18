cf049c8fe61dbed6460b74cf8c9b1b29f1a16479
sandhyarani8080 <sandhyaranijagadale@gmail.com>
2024-12-19 02:42:03 +0530

    second commit

---
+++ frontend/jobfront/src/app/services/registration.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Registration } from '../models/registration';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  serverurl="http://localhost:9090/registration"
+
+  constructor(private http:HttpClient) { }
+  getAll():Observable<Registration[]>{
+   return this.http.get<Registration[]>(this.serverurl+"/registrations")
+   
+  }
+  save(r:Registration):Observable<Registration>
+  {
+    return this.http.post<Registration>(this.serverurl+"/addreg",r)
+  }
+}
